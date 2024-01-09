39d3f3cc362308471933abc2f8bd3059a480004c
Raul Marcano Reimundez <rmarcano@c2r14s4.42madrid.com>
2024-01-09 15:08:07 +0100

    isalpha to strlen

---
+++ ft/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 14:46:16 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/09 15:06:17 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+int main()
+{
+	char *str;
+
+	str = "hola";
+
+	printf("%lu", ft_strlen(str));
+}*/
