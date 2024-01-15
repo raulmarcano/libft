3329a7c864871022bbf122a3207c575144b0844d
Raul Marcano Reimundez <rmarcano@c2r17s6.42madrid.com>
2024-01-15 20:36:25 +0100

    strnstr

---
+++ ft/ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 17:36:03 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/15 20:35:53 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (i < len && haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] && haystack[i] && needle[j] == haystack[i])
+		{
+			i++;
+			j++;
+		}
+		if (needle[j] == '\0')
+			return ((char *)haystack[i - j]);
+		i = i - j + 1;
+	}
+	return (0);
+}
