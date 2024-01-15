8a13b536c29e39398b2fdb40268b79fc555bf8b8
Raul Marcano Reimundez <rmarcano@c2r17s6.42madrid.com>
2024-01-15 12:44:16 +0100

    strchr

---
+++ ft/ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 11:51:40 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/15 12:41:06 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)(s + i));
+	return (NULL);
+}
+/*
+int	main()
+{
+	char	str[] = "hello, world!";
+	char	c = '\0';
+	ft_strchr(str, c);
+
+	return (0);
+}*/
