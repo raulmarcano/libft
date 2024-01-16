637144da370ff5c333d9d2d7f657e78a389ae3b2
Raul Marcano Reimundez <rmarcano@c2r13s5.42madrid.com>
2024-01-16 11:41:49 +0100

    atoi

---
+++ ft/ft_atoi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 11:15:25 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/16 11:41:29 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '+')
+	   i++;
+	while (str[i] ==  '-')
+	{
+		sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0')
+		i++;
+	}
+	return (result * sign);
+}
