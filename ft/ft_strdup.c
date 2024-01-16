7892f8b49a5acbc9163f361bca50c539c8a2a652
Raul Marcano Reimundez <rmarcano@c2r18s6.42madrid.com>
2024-01-16 14:18:24 +0100

    strdup

---
+++ ft/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 13:50:13 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/16 14:18:08 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strdup(const char *s1)
+{
+	int		size;
+	int		i;
+	char	*arr;
+
+	if (!s1)
+		return (0);
+	size = (ft_strlen(s1) + 1);
+	arr = (char *)malloc(size * sizeof(char));
+	i = 0;
+	if (!arr)
+		return (0);
+	while (i < size)
+	{
+		arr[i] = s1[i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
