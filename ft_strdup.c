e4460cef4da4d5b31c6cc770746e1e9ba2c2c3f6
Raul Marcano Reimundez <rmarcano@c2r17s6.42madrid.com>
2024-01-18 14:01:10 +0100

    Fix strdupdeleted null comprobation, it should segfault

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:50:13 by rmarcano          #+#    #+#             */
-/*   Updated: 2024/01/17 12:24:52 by rmarcano         ###   ########.fr       */
+/*   Updated: 2024/01/18 13:58:26 by rmarcano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,8 +17,6 @@ char	*ft_strdup(const char *s1)
 	size_t		i;
 	char		*arr;
 
-	if (!s1)
-		return (0);
 	size = (ft_strlen(s1) + 1);
 	arr = (char *)malloc(size * sizeof(char));
 	i = 0;
