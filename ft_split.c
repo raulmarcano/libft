5fb3ccab766e67ccc0a69100799946ee70e3da25
Raul Marcano Reimundez <rmarcano@c2r3s2.42madrid.com>
2024-01-24 14:03:51 +0100

    split imcomplete

--- ft_split.c
+++ ft_split.c
@@ -6,25 +6,101 @@
 /*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 18:54:32 by rmarcano          #+#    #+#             */
-/*   Updated: 2024/01/22 20:12:19 by rmarcano         ###   ########.fr       */
+/*   Updated: 2024/01/24 14:02:16 by rmarcano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char **ft_split(char const *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char **p;
-	int i;
-	int j;
-	
-	i = 0;
-	while (!s[i])
+	char	*sub;
+	size_t	i;
+
+	if (!s || start > ft_strlen(s))
 	{
-		if (s[i] == c)
-			
-			
+		sub = (char *)(malloc(1 * sizeof(char)));
+		sub[0] = '\0';
+		return (sub);
+	}
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = (char *)(malloc(sizeof(char) * (len + 1)));
+	if (sub == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i] != '\0')
+	{
+		sub[i] = s[start + i];
 		i++;
 	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+int	words(char *s, char c)
+{
+	int	count;
+	int	i;
+	
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (!(s[i] == c) && (s[i + 1] == c || s[i + 1] == '\0'))
+				count++;
+		i++;
+	}
+	return (count);
+}
+
+
+
+
+
+void	divide(char **strings, *s, char c, int count)
+{
+	int	start;
+	int	end;
+	int	i;
+	int	j;
+	int	cap;
+
+	cap = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		i = 0;
+		if (!s[i] == c && cap == 0)
+		{
+			start = i;
+			cap = 1;
+		}
+		if (!(s[i] == c) && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			end = i;
+			cap = 0;
+		}
+		while (j <= count)
+			strings[j++] =  t_substr(s, start, end - start + 1);
+		i++;
+	}
+}
+
+
+
+
+
+char **ft_split(char const *s, char c);
+{
+	char **strings;
+	int	count;
+	
+	count = words(s, c);
+	strings = (char **)malloc((count + 1)* sizeof(void *));
+	if (!strings)
+		return (NULL);
+	divide(strings, (char *)s, c, count);
+	return (strings);
 }
 
 int	main()
