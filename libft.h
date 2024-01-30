407733076728b4e26fa6eb8af177d63e5a9ca7b6
Raul Marcano Reimundez <rmarcano@c2r3s3.42madrid.com>
2024-01-30 18:25:59 +0100

    striteri, missing some test

--- libft.h
+++ libft.h
@@ -47,5 +47,5 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 #endif
