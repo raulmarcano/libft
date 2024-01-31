471e3252cf9ae9b18b3cc9492653ca5655b55a04
Raul Marcano Reimundez <rmarcano@c2r3s3.42madrid.com>
2024-01-31 18:17:54 +0100

    putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 18:03:59 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/01/31 18:04:03 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	digit;
+
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = n * -1;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	digit = '0' + (n % 10);
+	write(fd, &digit, 1);
+	return ;
+}
+/*
+int main()
+{
+   
+    int fd = 1;  
+    ft_putnbr_fd(-123, fd);
+
+    return 0;
+}
+*/
