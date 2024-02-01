4b95ab8de1c6b68169252dc577068373caafd610
Raul Marcano Reimundez <rmarcano@c2r2s1.42madrid.com>
2024-02-01 17:56:30 +0100

    added lstnew on libft

--- libft.h
+++ libft.h
@@ -58,6 +58,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-t_list  *ft_lstnew(void *content);
+t_list  *ft_lstnew_bonus(void *content);
 
 #endif
