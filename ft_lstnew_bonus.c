2d8254d787b5a0cb3d021d7c3238e2c8cd4b7390
Raúl Marcano <raulmarcano@MacBook-Pro-de-Irene.local>
2024-02-05 14:13:34 +0100

    lstnew not tested

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -12,11 +12,11 @@
 
 #include "libft.h"
 
-t_list	*ft_lstnew_bonus(void *content)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*list;
 
-	list = (t_list *)malloc(sizeof(t_list));
+	list = malloc(sizeof(t_list));
 	if (!list)
 		return (NULL);
 	list->content = content;
