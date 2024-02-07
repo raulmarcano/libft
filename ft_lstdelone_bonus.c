795fd85dfd09e8b4730a2e85c918aa74693f61f8
Raúl Marcano <raulmarcano@MacBook-Pro-de-Irene.local>
2024-02-07 20:41:14 +0100

    lstdelone

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarcano <rmarcano@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 20:16:53 by rmarcano          #+#    #+#             */
+/*   Updated: 2024/02/07 20:17:15 by rmarcano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
